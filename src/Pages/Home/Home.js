4d8d7a5e5512ac177bd0435fe56d3c91911290ea
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-06-05 02:31:09 +0600

    Initial setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../../Common/PageTitle';
+
+const Home = () => {
+    return (
+        <div>
+            <PageTitle title="Home" />
+            <h1>Hello every one</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../../Common/PageTitle';
+
+const Login = () => {
+    return (
+        <div>
+            <PageTitle title="Login" />
+            <h1>Pleae login</h1>
+        </div>
+    );
+};
+
+export default Login;
