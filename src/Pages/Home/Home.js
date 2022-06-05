c70311331bb1f1b9cc323b1ea13781b6e2c02460
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-06-05 11:19:27 +0600

    Navbar Done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageTitle from '../../Common/PageTitle';
+import Navbar from './Navbar';
 
 const Home = () => {
     return (
         <div>
             <PageTitle title="Home" />
+            <Navbar />
             <h1>Hello every one</h1>
         </div>
     );
